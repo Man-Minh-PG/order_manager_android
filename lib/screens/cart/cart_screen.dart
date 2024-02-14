59c2f8e3dbb697615098a32c6d79c3b88e970564
[Lampart Bui Minh Man] <minh_man@lampart-vn.com>
2024-02-14 21:36:51 +0700

    screen order detail

--- lib/screens/cart/cart_screen.dart
+++ lib/screens/cart/cart_screen.dart
@@ -1,100 +1,165 @@
-import 'package:flutter/material.dart';
-import 'package:grocery_app/common_widgets/app_button.dart';
-import 'package:grocery_app/helpers/column_with_seprator.dart';
-import 'package:grocery_app/models/grocery_item.dart';
-import 'package:grocery_app/widgets/chart_item_widget.dart';
+// import 'package:flutter/material.dart';
+// import 'package:grocery_app/common_widgets/app_button.dart';
+// import 'package:grocery_app/helpers/column_with_seprator.dart';
+// import 'package:grocery_app/models/grocery_item.dart';
+// import 'package:grocery_app/widgets/chart_item_widget.dart';
 
-import 'checkout_bottom_sheet.dart';
+// import 'checkout_bottom_sheet.dart';
+
+// class CartScreen extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       body: SafeArea(
+//         child: SingleChildScrollView(
+//           child: Column(
+//             children: [
+//               SizedBox(
+//                 height: 25,
+//               ),
+//               Text(
+//                 "List Order",
+//                 style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+//               ),
+//               SizedBox(
+//                 height: 20,
+//               ),
+//               Column(
+//                 children: getChildrenWithSeperator(
+//                   addToLastChild: false,
+//                   widgets: demoItems.map((e) {
+//                     return Container(
+//                       padding: EdgeInsets.symmetric(
+//                         horizontal: 25,
+//                       ),
+//                       width: double.maxFinite,
+//                       child: ChartItemWidget(
+//                         item: e,
+//                       ),
+//                     );
+//                   }).toList(),
+//                   seperator: Padding(
+//                     padding: const EdgeInsets.symmetric(
+//                       horizontal: 25,
+//                     ),
+//                     child: Divider(
+//                       thickness: 1,
+//                     ),
+//                   ),
+//                 ),
+//               ),
+//               Divider(
+//                 thickness: 1,
+//               ),
+//               getCheckoutButton(context)
+//             ],
+//           ),
+//         ),
+//       ),
+//     );
+//   }
+
+//   Widget getCheckoutButton(BuildContext context) {
+//     return Container(
+//       padding: EdgeInsets.symmetric(horizontal: 25, vertical: 20),
+//       child: AppButton(
+//         label: "Go To Check Out",
+//         fontWeight: FontWeight.w600,
+//         padding: EdgeInsets.symmetric(vertical: 30),
+//         trailingWidget: getButtonPriceWidget(),
+//         onPressed: () {
+//           showBottomSheet(context);
+//         },
+//       ),
+//     );
+//   }
+
+//   Widget getButtonPriceWidget() {
+//     return Container(
+//       padding: EdgeInsets.all(2),
+//       decoration: BoxDecoration(
+//         color: Color(0xff489E67),
+//         borderRadius: BorderRadius.circular(4),
+//       ),
+//       child: Text(
+//         "\$12.96",
+//         style: TextStyle(fontWeight: FontWeight.w600),
+//       ),
+//     );
+//   }
+
+//   void showBottomSheet(context) {
+//     showModalBottomSheet(
+//         context: context,
+//         isScrollControlled: true,
+//         backgroundColor: Colors.transparent,
+//         builder: (BuildContext bc) {
+//           return CheckoutBottomSheet();
+//         });
+//   }
+// }
+
+
+import 'package:flutter/material.dart';
 
 class CartScreen extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: SafeArea(
-        child: SingleChildScrollView(
+  // const CartScreen({Key key}) : super(key: key);
+
+  @override 
+   Widget build(BuildContext context) {
+    final List<String> phoneNumber = <String>[
+      '6666677897',
+      '7777777777',
+      '3498789678',
+      '7897989780'
+    ];
+    final List<String> callType = <String>[
+      "Incoming",
+      "Outgoing",
+      "Incoming",
+      "Missed"
+    ];
+ 
+    return ListView.separated(
+      shrinkWrap: true,
+      padding: const EdgeInsets.all(8),
+      itemCount: phoneNumber.length,
+      itemBuilder: (BuildContext context, int index) {
+        return Card(
+          color: Colors.white,
+          borderOnForeground: true,
+          elevation: 10,
           child: Column(
-            children: [
-              SizedBox(
-                height: 25,
-              ),
-              Text(
-                "List Order",
-                style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-              ),
-              SizedBox(
-                height: 20,
-              ),
-              Column(
-                children: getChildrenWithSeperator(
-                  addToLastChild: false,
-                  widgets: demoItems.map((e) {
-                    return Container(
-                      padding: EdgeInsets.symmetric(
-                        horizontal: 25,
-                      ),
-                      width: double.maxFinite,
-                      child: ChartItemWidget(
-                        item: e,
-                      ),
-                    );
-                  }).toList(),
-                  seperator: Padding(
-                    padding: const EdgeInsets.symmetric(
-                      horizontal: 25,
-                    ),
-                    child: Divider(
-                      thickness: 1,
-                    ),
-                  ),
+            mainAxisSize: MainAxisSize.min,
+            children: <Widget>[
+              ListTile(
+                leading: Icon(Icons.call),
+                title: Text("${phoneNumber[index]}",
+                    style: TextStyle(color: Colors.green)),
+                subtitle: Text(
+                  "${callType[index]}",
+                  style: TextStyle(color: Colors.orangeAccent),
                 ),
               ),
-              Divider(
-                thickness: 1,
+              Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: <Widget>[
+                  TextButton(
+                    child: const Text('Dail'),
+                    onPressed: () {/* ... */},
+                  ),
+                  const SizedBox(width: 8),
+                  TextButton(
+                    child: const Text('Call History'),
+                    onPressed: () {/* ... */},
+                  ),
+                ],
               ),
-              getCheckoutButton(context)
             ],
           ),
-        ),
-      ),
+        );
+      },
+      separatorBuilder: (BuildContext context, int index) => const Divider(),
     );
-  }
-
-  Widget getCheckoutButton(BuildContext context) {
-    return Container(
-      padding: EdgeInsets.symmetric(horizontal: 25, vertical: 20),
-      child: AppButton(
-        label: "Go To Check Out",
-        fontWeight: FontWeight.w600,
-        padding: EdgeInsets.symmetric(vertical: 30),
-        trailingWidget: getButtonPriceWidget(),
-        onPressed: () {
-          showBottomSheet(context);
-        },
-      ),
-    );
-  }
-
-  Widget getButtonPriceWidget() {
-    return Container(
-      padding: EdgeInsets.all(2),
-      decoration: BoxDecoration(
-        color: Color(0xff489E67),
-        borderRadius: BorderRadius.circular(4),
-      ),
-      child: Text(
-        "\$12.96",
-        style: TextStyle(fontWeight: FontWeight.w600),
-      ),
-    );
-  }
-
-  void showBottomSheet(context) {
-    showModalBottomSheet(
-        context: context,
-        isScrollControlled: true,
-        backgroundColor: Colors.transparent,
-        builder: (BuildContext bc) {
-          return CheckoutBottomSheet();
-        });
   }
 }
